1760b3c717b99ae2ddd9b405d65feb28e39a83ab
kanakanho <kahoshibakichi@gmail.com>
2024-09-28 05:48:24 +0900

    feat: googleアナリティクス用の設定の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import react from "@astrojs/react";
 // @ts-check
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     css: {
       preprocessorOptions: {
