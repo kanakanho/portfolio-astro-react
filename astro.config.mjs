b6dcf734389f4e8d7fc7d2893796e42af9591b8e
kanakanho <kahoshibakichi@gmail.com>
2024-09-28 05:54:50 +0900

    feat: vercelアナリティクス用の設定の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import react from "@astrojs/react";
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [react()],
   vite: {
     css: {
